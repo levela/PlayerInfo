6124e1365678ed3168f1a6510246f5cc2892a642
Koen Basten <kbasten@gmail.com>
2013-06-23 11:45:27 +0200

    WebClientTimeOut added

--- PlayerInfo/PlayerInfo/PlayerInfo.cs
+++ PlayerInfo/PlayerInfo/PlayerInfo.cs
@@ -69,36 +69,13 @@ namespace PlayerInfo
 							String opponentName = getOpponentName(gm);
 
 							// now use the api to get the player's data
-							String html = new WebClient().DownloadString("http://a.scrollsguide.com/player?fields=all&name=" + opponentName);
-
-							// convert the html data to ApiResultMessage
-							JsonReader reader = new JsonReader();
-							Message message = reader.Read(html, System.Type.GetType("ApiResultMessage")) as ApiResultMessage;
-
-							ApiResultMessage armsg = (ApiResultMessage)message;
-
-							String chatMsg = "";
-							if (armsg.msg.Equals("success")) // api call succeeded
-							{
-								chatMsg += "Player info: " + armsg.data.name + "\n";
-								chatMsg += "Rank: " + armsg.data.rank + "\n";
-								chatMsg += "Rating: " + armsg.data.rating + "\n";
-								chatMsg += "Games played: " + armsg.data.played + "\n";
-								chatMsg += "Games won: " + armsg.data.won + " (" + Math.Round(((float)armsg.data.won / (float)armsg.data.played) * 100) + "%)\n";
-							}
-							else
-							{
-								chatMsg = "Couldn't get data for " + opponentName + ".";
-							}
-							MethodInfo mi = typeof(BattleMode).GetMethod("updateChat", BindingFlags.NonPublic | BindingFlags.Instance);
-
-							if (mi != null) // send chat message
-							{
-								mi.Invoke(bm, new String[] { chatMsg });
-							}
-							else // can't invoke updateChat
-							{
-							}
+							WebClientTimeOut wc = new WebClientTimeOut();
+							wc.TimeOut = 3000;
+							wc.DownloadStringCompleted += (sender, e) =>
+								{
+									parseJSONResult(e.Result, opponentName);
+								};
+							wc.DownloadStringAsync(new Uri("http://a.scrollsguide.com/player?fields=all&name=" + opponentName));
 						}
 						catch // could not get information
 						{
@@ -120,6 +97,38 @@ namespace PlayerInfo
 		private String getOpponentName(GameInfoMessage gm)
 		{
 			return (gm.color == TileColor.white) ? gm.black : gm.white;
+		}
+
+		private void parseJSONResult(String result, String oppName)
+		{
+			// convert the html data to ApiResultMessage
+			JsonReader reader = new JsonReader();
+			Message message = reader.Read(result, System.Type.GetType("ApiResultMessage")) as ApiResultMessage;
+
+			ApiResultMessage armsg = (ApiResultMessage)message;
+
+			String chatMsg = "";
+			if (armsg.msg.Equals("success")) // api call succeeded
+			{
+				chatMsg += "Player info: " + armsg.data.name + "\n";
+				chatMsg += "Rank: " + armsg.data.rank + "\n";
+				chatMsg += "Rating: " + armsg.data.rating + "\n";
+				chatMsg += "Games played: " + armsg.data.played + "\n";
+				chatMsg += "Games won: " + armsg.data.won + " (" + Math.Round(((float)armsg.data.won / (float)armsg.data.played) * 100) + "%)\n";
+			}
+			else
+			{
+				chatMsg = "Couldn't get data for " + oppName + ".";
+			}
+			MethodInfo mi = typeof(BattleMode).GetMethod("updateChat", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			if (mi != null) // send chat message
+			{
+				mi.Invoke(bm, new String[] { chatMsg });
+			}
+			else // can't invoke updateChat
+			{
+			}
 		}
 	}
 }
---
+++ PlayerInfo/PlayerInfo/WebClientTimeOut.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace PlayerInfo
+{
+	public class WebClientTimeOut : WebClient
+	{
+		protected override WebRequest GetWebRequest(Uri address)
+		{
+			WebRequest webRequest = base.GetWebRequest(address);
+			webRequest.Timeout = TimeOut;
+			return webRequest;
+		}
+		public int TimeOut
+		{
+			set;
+			get;
+		}
+	}
+}
